77da3dab911da9a66b6bf4ab565a8dc2b3cf233d
Gideon Amienghemhen <amienz0958@gmail.com>
2025-04-02 18:11:06 +0100

    Configuring settings.py for Render

--- legal_platform/settings.py
+++ legal_platform/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*w5=6sacm%79me_wcjj1-znri_o#%cqtr=_d_q#!1r0)c_p((0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["legal-platform.onrender.com"]
 
 
 # Application definition
@@ -51,6 +51,7 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
